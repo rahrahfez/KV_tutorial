e6521ddb4a6f520a43eba471cc9060d24b3bbef2
David <rahrahfez@hotmail.com>
2020-08-22 12:06:11 -0400

    initial commit

---
+++ lib/kv.ex
@@ -0,0 +1,11 @@
+defmodule KV do
+  use Application
+  @moduledoc """
+  Documentation for `KV`.
+  """
+  
+  @impl true
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
+  end
+end
